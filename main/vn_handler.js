3196ac4bb4996078ad7f050da1bbee29a8118432
nalloway-slu <nalloway@slu.edu>
2023-12-06 22:35:48 -0600

    Working on a parser-less version of the project

--- main/vn_handler.js
+++ main/vn_handler.js
@@ -4,29 +4,6 @@ vn_handler.js - Defines how to interpret text files in `assets` folder containin
 TO DO: Finish rest of documentation!
 ****************/
 
-let VN_List_Of_Backgrounds = [];
-let VN_List_Of_Characters = [];
-let VN_List_Of_Flags = [];
-let VN_List_Of_Scenes = [];
-
-function add_vn_background_to_list (bg) {
-  VN_List_Of_Backgrounds[bg.name] = bg;
-}
-
-function add_vn_character_to_list (char) {
-  VN_List_Of_Characters[char.get_name()] = char;
-}
-
-function add_vn_flag_to_list (flag) {
-
-}
-
-function add_vn_scene_to_list (scene) {
-
-}
-
-//-------------------------------------------------------------------------------------------------------------------------------------------------
-
 function handler () {
 
 }
